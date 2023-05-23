5602333c1427436419f5ccb4527ce5b19999a217
barmud3 <130641348+barmud3@users.noreply.github.com>
2023-05-23 12:46:10 +0300

    adding doc to st_reactor.cpp

--- st_reactor.cpp
+++ st_reactor.cpp
@@ -1,12 +1,4 @@
 #include "st_reactor.hpp"
-#include <iostream>
-#include <vector>
-#include <pthread.h>
-#include <poll.h>
-#include <unistd.h>
-#include <functional>
-#include <algorithm>
-
 
 void *createReactor(){
     pReactor ourReactor = (pReactor)malloc(sizeof(Reactor) * 1);
@@ -17,13 +9,37 @@ void *createReactor(){
     return ourReactor;
 }
 
+void addFd(void* thisPtr, int fd, handler_t handler) {
+
+    // Push the pair to the pairs vector for (fd,func)
+    ((pReactor)thisPtr)->fd_handlers.push_back({fd, handler});
+    // Push only the fd to the Fds vector
+    ((pReactor)thisPtr)->poll_fds.push_back({fd, POLLIN, 0 });
+    
+}
+
+void startReactor(void *thisPtr){
+    if (!(((pReactor)thisPtr)->active)) {
+            ((pReactor)thisPtr)->active = true;
+            // Start the reactor thread within reactorLoop function
+            pthread_create(((pReactor)thisPtr)->reactor_thread, nullptr, &reactorLoop, thisPtr);
+    }   
+}
+
 void stopReactor(void *thisPtr){
     pthread_cancel(*(((pReactor)thisPtr)->reactor_thread));
+}
+
+void WaitFor(void * thisPtr){
+    if (((pReactor)thisPtr)->active) {
+        pthread_join((*((pReactor)thisPtr)->reactor_thread), nullptr);
+    }
 }
 
 void* reactorLoop(void* arg) {
     
     Reactor* reactor = static_cast<Reactor*>(arg);
+
     while (reactor->active) {
         
         int num_ready_fds = poll(reactor->poll_fds.data(), reactor->poll_fds.size(), -1);
@@ -31,43 +47,18 @@ void* reactorLoop(void* arg) {
             std::cerr << "Error in poll\n";
             break;
         }
+
+        // Running thorugh the fds data strcture to find an "HOT" fd. if  found, execute the function belongs to the fd
         for (const auto& poll_fd : reactor->poll_fds) {
             if (poll_fd.revents & POLLIN) {
                 int fd = poll_fd.fd;
                 auto it = std::find_if(reactor->fd_handlers.begin(), reactor->fd_handlers.end(),
                 [fd](const std::pair<int, handler_t>& fd_handler) { return fd_handler.first == fd; });
-            if (it != reactor->fd_handlers.end()) {
-                it->second(fd);
-            }
-
+                if (it != reactor->fd_handlers.end()) {
+                    it->second(fd);
+                }
             }
         }
     }
-
     pthread_exit(nullptr);
-}
-
-void startReactor(void *thisPtr){
-    if (!(((pReactor)thisPtr)->active)) {
-            ((pReactor)thisPtr)->active = true;
-            pthread_create(((pReactor)thisPtr)->reactor_thread, nullptr, &reactorLoop, thisPtr);
-        
-        ///////////////////
-    }
-    
-    
-}
-
-void addFd(void* thisPtr, int fd, handler_t handler) {
-    ((pReactor)thisPtr)->fd_handlers.push_back({fd, handler});
-    ((pReactor)thisPtr)->poll_fds.push_back({fd, POLLIN, 0 });
-    
-}
-
-
-void WaitFor(void * thisPtr){
-    if (((pReactor)thisPtr)->active) 
-    {
-        pthread_join((*((pReactor)thisPtr)->reactor_thread), nullptr);
-    }
 }
